80e4e225eb116e751c7901acca8cc454962cefab
rsuri22 <rohansuri.prime@gmail.com>
2022-11-05 23:31:36 -0500

    A few of the structures are rendering now

--- src/main/java/com/example/chickennugget/Linear.java
+++ src/main/java/com/example/chickennugget/Linear.java
@@ -1,6 +1,6 @@
 package com.example.chickennugget;
 
-import javafx.animation.FillTransition;
+
 import javafx.application.Application;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
@@ -12,28 +12,19 @@ import javafx.scene.Scene;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
-import javafx.scene.shape.Box;
 import javafx.scene.shape.Cylinder;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Sphere;
 import javafx.scene.transform.Rotate;
-import javafx.scene.transform.Scale;
+import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
-import javafx.scene.control.Slider;
-import javafx.animation.Timeline;
-import javafx.animation.ParallelTransition;
-import javafx.animation.RotateTransition;
-import javafx.animation.ScaleTransition;
-import javafx.animation.TranslateTransition;
-import javafx.util.Duration;
-import java.io.IOException;
-import java.io.SerializablePermission;
 
-public class HelloApplication extends Application {
+
+public class Linear extends Application {
 
     public static final float WIDTH = 1400;
     public static final float HEIGHT = 800;
-    public static final float RADIUS = 50;
+    public static final float RADIUS = 25;
+    public static final float CYLHEIGHT = RADIUS * 3;
     private double anchorX, anchorY;
     private double anchorAngleX = 0;
     private double anchorAngleY = 0;
@@ -43,42 +34,52 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Sphere sphere1 = new Sphere(RADIUS);
-        sphere1.translateYProperty().set(RADIUS * 6);
-        colorSphere(sphere1, Color.GREEN);
+        final PhongMaterial bondMaterial = new PhongMaterial();
+        bondMaterial.setDiffuseColor(Color.DARKGRAY);
+        bondMaterial.setSpecularColor(Color.GRAY);
 
-        Sphere sphere2 = new Sphere(RADIUS);
-        colorSphere(sphere2, Color.RED);
+        final PhongMaterial atom1Material = new PhongMaterial();
+        atom1Material.setDiffuseColor(Color.DARKRED);
+        atom1Material.setSpecularColor(Color.RED);
 
-        Sphere sphere3 = new Sphere(RADIUS);
-        sphere3.translateXProperty().set(RADIUS * 6);
-        colorSphere(sphere3, Color.MAGENTA);
-
-        Cylinder cylinder1 = new Cylinder(RADIUS/5, RADIUS * 4);
-        cylinder1.translateYProperty().set(RADIUS * 3);
-
-        Cylinder cylinder2 = new Cylinder(RADIUS/5, RADIUS * 4);
-        cylinder2.translateXProperty().set(RADIUS * 3);
-        cylinder2.setRotationAxis(Rotate.Z_AXIS);
-        cylinder2.rotateProperty().set(90);
-
-        Cylinder cylinder3 = new Cylinder(RADIUS/5, RADIUS * 4);
-        cylinder3.translateXProperty().set(RADIUS * -3);
-        cylinder3.setRotationAxis(Rotate.Z_AXIS);
-        cylinder3.rotateProperty().set(90);
+        final PhongMaterial atom2Material = new PhongMaterial();
+        atom2Material.setDiffuseColor(Color.WHITE);
+        atom2Material.setSpecularColor(Color.LIGHTBLUE);
 
 
-        Group group = new Group();
-        group.getChildren().add(sphere1);
-        group.getChildren().add(cylinder1);
-        group.getChildren().add(sphere2);
-        group.getChildren().add(sphere3);
-        group.getChildren().add(cylinder2);
-        group.getChildren().add(cylinder3);
 
+        Sphere centralAtom = new Sphere(RADIUS);
+        centralAtom.setMaterial(atom1Material);
+
+        Sphere outerAtom1 = new Sphere(RADIUS);
+        outerAtom1.translateXProperty().set(CYLHEIGHT);
+        outerAtom1.setMaterial(atom2Material);
+
+        Sphere outerAtom2 = new Sphere(RADIUS);
+        outerAtom2.translateXProperty().set(-CYLHEIGHT);
+        outerAtom2.setMaterial(atom2Material);
+
+
+
+
+
+
+
+
+
+
+        Cylinder bond1 = createConnection((new Point3D(0, 0, 0)), new Point3D(CYLHEIGHT, 0, 0));
+        bond1.setMaterial(bondMaterial);
+
+        Cylinder bond2 = createConnection((new Point3D(0, 0, 0)), new Point3D(-CYLHEIGHT, 0, 0));
+        bond2.setMaterial(bondMaterial);
+
+
+
+        Group group = new Group(centralAtom, outerAtom1, outerAtom2, bond2, bond1);
 
         Camera camera = new PerspectiveCamera();
-        Scene scene = new Scene(group, WIDTH, HEIGHT);
+        Scene scene = new Scene(group, WIDTH, HEIGHT, true);
         scene.setFill(Color.CORNFLOWERBLUE);
         scene.setCamera(camera);
 
@@ -91,13 +92,25 @@ public class HelloApplication extends Application {
         primaryStage.setTitle("Structure Generation");
         primaryStage.setScene(scene);
         primaryStage.show();
-        group.setRotationAxis(new Point3D(500, 350, 0));
     }
 
-    private Sphere colorSphere(Sphere s, Color c){
-        PhongMaterial color = new PhongMaterial(c);
-        s.setMaterial(color);
-        return s;
+    public Cylinder createConnection(Point3D origin, Point3D target) {
+        Point3D yAxis = new Point3D(0, 1, 0);
+        Point3D diff = target.subtract(origin);
+        double height = diff.magnitude();
+
+        Point3D mid = target.midpoint(origin);
+        Translate moveToMidpoint = new Translate(mid.getX(), mid.getY(), mid.getZ());
+
+        Point3D axisOfRotation = diff.crossProduct(yAxis);
+        double angle = Math.acos(diff.normalize().dotProduct(yAxis));
+        Rotate rotateAroundCenter = new Rotate(-Math.toDegrees(angle), axisOfRotation);
+
+        Cylinder line = new Cylinder(RADIUS/5, height);
+
+        line.getTransforms().addAll(moveToMidpoint, rotateAroundCenter);
+
+        return line;
     }
 
 
