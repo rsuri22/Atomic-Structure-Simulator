a3150fac034828ac2f564473ee4d7d582bd36c7d
rsuri22 <rohansuri.prime@gmail.com>
2022-11-09 16:08:38 +0000

    feat: add main class to run program

---
+++ src/main/java/com/example/chickennugget/Main.java
@@ -0,0 +1,10 @@
+package com.example.chickennugget;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(Octahedral.class, args);
+    }
+    
+}
